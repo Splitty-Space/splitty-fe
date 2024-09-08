6919037e016c6bb833a29c30b5dce9e1496998e6
eoller <Egorpyp@gmail.com>
2024-09-09 00:33:08 +0200

    changed /searchFriends -> /searchUser for search

--- services/useSearchFriends.ts
+++ services/useSearchFriends.ts
@@ -1,5 +1,4 @@
 import useAxios from "axios-hooks";
-import getCurrentUserId from "@/utils/getCurrentUserId";
 
 export const useSearchFriends = (searchValue: string) => {
     const query = searchValue.trim()
@@ -7,9 +6,8 @@ export const useSearchFriends = (searchValue: string) => {
     let doRequest = query === "" || query.length < 3;
 
     const [{data, loading, error}, refetch] = useAxios({
-        url: "/searchFriends",
+        url: "/searchUser",
         params: {
-            user_id: getCurrentUserId(),
             page: 0,
             per_page: 10,
             query: query,
