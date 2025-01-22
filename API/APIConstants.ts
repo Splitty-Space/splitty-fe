b5e70c7461416178f5609721bf06726557d5f846
Mikhail Radziukevich <m.radziukevich@dealcloud.com>
2025-01-22 10:59:05 +0100

    Add workflow and Dockerfile

--- API/APIConstants.ts
+++ API/APIConstants.ts
@@ -1,2 +1,2 @@
 export const MASTER_USER_ID = 254960854;
-export const SERVER_URL = "http://127.0.0.1:8000"; // 'http://127.0.0.1:8000';
+export const SERVER_URL = process.env.NEXT_SERVER_URL || "http://127.0.0.1:8000"; // 'http://127.0.0.1:8000';
---
+++ Dockerfile
@@ -0,0 +1,16 @@
+# Base image
+FROM node:18-alpine
+
+# Set working directory
+WORKDIR /app
+
+COPY . .
+
+RUN yarn install
+RUN yarn build
+
+# Expose the port for the React dev server
+EXPOSE 3000
+
+# Start the React app
+CMD ["yarn", "start"]
