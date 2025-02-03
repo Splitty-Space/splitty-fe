d985321bfedb582fad9796fdd84ed2f3bcee419c
Mikhail Radziukevich <m.radziukevich@dealcloud.com>
2025-02-03 19:30:10 +0100

    Add ModalHeader

--- app/main/friends/expenseDetails/ExpenseHistoryModal.tsx
+++ app/main/friends/expenseDetails/ExpenseHistoryModal.tsx
@@ -3,6 +3,9 @@ import {Modal} from "@telegram-apps/telegram-ui";
 import ExpenseHistory from "@/app/main/friends/expenseDetails/ExpenseHistory";
 import Activity from "@/entities/Activity";
 import {Expense} from "@/entities";
+import {
+    ModalHeader
+} from "@telegram-apps/telegram-ui/dist/components/Overlays/Modal/components/ModalHeader/ModalHeader";
 
 export const ExpenseHistoryModal = ({trigger, activity, selectedExpense}: Readonly<{
     trigger: React.ReactNode,
@@ -10,7 +13,12 @@ export const ExpenseHistoryModal = ({trigger, activity, selectedExpense}: Readon
     selectedExpense: Expense,
 }>) => {
     return (
-        <Modal className="z-50 top-20" trigger={trigger} aria-describedby={undefined}>
+        <Modal
+            header={<ModalHeader>Only iOS header</ModalHeader>}
+            className="z-50 top-20"
+            trigger={trigger}
+            aria-describedby={undefined}
+        >
             <ExpenseHistory
                 selectedExpense={selectedExpense}
                 activity={activity}
