b7a415c0635836d885ac482e1c917e3479b142ae
Mikhail Radziukevich <m.radziukevich@dealcloud.com>
2025-02-17 17:16:30 +0100

    User id sdk fix

--- utils/getCurrentUserId.ts
+++ utils/getCurrentUserId.ts
@@ -1,20 +1,15 @@
 import {MASTER_USER_ID} from "@/API/APIConstants";
 import {isTMA} from "@telegram-apps/bridge";
-import {retrieveLaunchParams} from "@telegram-apps/sdk";
+import {initData} from "@telegram-apps/sdk";
 
 export default function getCurrentUserId() {
-    // @ts-ignore
-
     console.log("global?.window ", global?.window);
 
     // @ts-ignore
     if (global?.window && window && isTMA("simple")) {
-        const {initDataRaw, initData} = retrieveLaunchParams();
-        console.log("initDataRaw = ", initDataRaw);
         console.log("initData = ", initData);
 
-        // @ts-ignore
-        const user = initData?.user;
+        const user = initData.user();
         console.log("user = ", user);
 
         return user?.id || MASTER_USER_ID;
