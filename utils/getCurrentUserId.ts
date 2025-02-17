287555826f6d50b46d3717f3e4405346d3f3b963
Mikhail Radziukevich <m.radziukevich@dealcloud.com>
2025-02-17 16:05:50 +0100

    user id sdk fix

--- utils/getCurrentUserId.ts
+++ utils/getCurrentUserId.ts
@@ -1,6 +1,7 @@
 import {MASTER_USER_ID} from "@/API/APIConstants";
 import {initData} from "@telegram-apps/sdk";
 
+initData.restore();
 const user = initData.user();
 
 console.log("user = ", user);
