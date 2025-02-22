31780a662e3756257ea24b085d77849fe6f02ba0
Mikhail Radziukevich <m.radziukevich@dealcloud.com>
2025-02-23 00:12:40 +0100

    Enable fullscreen

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -84,7 +84,7 @@ export default function Page() {
             }
         };
 
-        // mountViewport().then(() => enableFullscreen()); // TODO fullscreen
+        mountViewport().then(() => enableFullscreen()); // TODO fullscreen
 
         return () => {
             viewport.unmount();
