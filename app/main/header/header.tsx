9eed3b2338650f09667020ff2da1a2f07d7de0e1
Mikhail Radziukevich <m.radziukevich@dealcloud.com>
2025-03-03 19:50:32 +0100

    Fix tg header

--- app/main/header/header.tsx
+++ app/main/header/header.tsx
@@ -26,10 +26,10 @@ const Header = forwardRef<HTMLDivElement, HeaderProps>(({
                                                             AfterComponent
                                                         }, ref) => {
     return (
-        <header ref={ref} className={classNames("h-16 fixed top-0 left-0 w-full z-40 header_blur", {
-            "padding-top-24": viewport.isFullscreen(),
-        }, className)}>
-            <FixedLayout vertical="top" className={classNames("px-4 z-10 pt-3", layoutClassName)}>
+        <header ref={ref} className={classNames("h-16 fixed top-0 left-0 w-full z-40 header_blur", className)}>
+            <FixedLayout vertical="top" className={classNames("px-4 z-10 pt-3", {
+                "padding-top-24": viewport.isFullscreen(),
+            }, layoutClassName)}>
                 <div className={classNames("flex shrink items-center justify-between mb-3", subHeaderClassName)}>
                     {LeftComponent && <LeftComponent/>}
 
