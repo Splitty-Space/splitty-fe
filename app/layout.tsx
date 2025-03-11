c7c840aad97cabfdc68559de5486c44341f7a9f9
Mikhail Radziukevich <m.radziukevich@dealcloud.com>
2025-03-11 20:10:31 +0100

    Add back button

--- app/layout.tsx
+++ app/layout.tsx
@@ -176,7 +176,7 @@ export default function RootLayout({children}: Readonly<{
 
     return (
         <html lang="en">
-        {/*<Script src="https://telegram.org/js/telegram-web-app.js"/>*/}
+        <Script src="https://telegram.org/js/telegram-web-app.js"/>
 
         <body className={classNames("overflow-hidden h-screen", {
             "body_dark": appearance === DARK
