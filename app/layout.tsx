4709f689e7967a45ca92cfdc8e4aa9b6e56ba13b
Mikhail Radziukevich <m.radziukevich@dealcloud.com>
2025-03-11 20:49:52 +0100

    Add back button

--- app/layout.tsx
+++ app/layout.tsx
@@ -164,7 +164,7 @@ export default function RootLayout({children}: Readonly<{
         }
 
         return () => {
-            offClick();
+            offClick && offClick();
         };
     }, [router, pathname]);
 
