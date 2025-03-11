e1857052178d1087ba4a05f4d748279a54c05f85
Mikhail Radziukevich <m.radziukevich@dealcloud.com>
2025-03-11 22:18:49 +0100

    Change Background color and Header color

--- app/layout.tsx
+++ app/layout.tsx
@@ -150,13 +150,13 @@ export default function RootLayout({children}: Readonly<{
 
         mountMiniApp().then(() => {
             if (miniApp.setBackgroundColor.isAvailable()) {
-                miniApp.setBackgroundColor("bg_color");
+                miniApp.setBackgroundColor("#1c1c1d");
 
                 console.log("miniApp.backgroundColor() = ", miniApp.backgroundColor());
             }
 
             if (miniApp.setHeaderColor.isAvailable()) {
-                miniApp.setHeaderColor("bg_color");
+                miniApp.setHeaderColor("#1c1c1d");
                 miniApp.headerColor();
 
                 console.log("miniApp.headerColor() = ", miniApp.headerColor());
