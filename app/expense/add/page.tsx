982722d111835ac37dd9a41755e232114c4e2e03
Mikhail Radziukevich <m.radziukevich@dealcloud.com>
2025-03-31 19:24:08 +0200

    Fix en input

--- app/expense/add/page.tsx
+++ app/expense/add/page.tsx
@@ -369,7 +369,7 @@ export default function AddExpense() {
                     <div className="flex items-center justify-between">
                         <div className="grow mr-1">
                             <Input
-                                type="text"
+                                type="number"
                                 inputMode="decimal"
                                 step="any"
                                 // @ts-ignore
