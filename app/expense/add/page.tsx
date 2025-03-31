46e98eee64fc0d7277b3d300657b5d07ccfcdeea
Mikhail Radziukevich <m.radziukevich@dealcloud.com>
2025-03-31 20:38:39 +0200

    Fix datetimepicker width

--- app/expense/add/page.tsx
+++ app/expense/add/page.tsx
@@ -396,7 +396,7 @@ export default function AddExpense() {
                                 disabled={!!selectedExpense}
                                 value={dayjs(date)}
                                 onChange={onDateChange}
-                                className="w-60 rounded-3xl"
+                                className="w-52 rounded-3xl"
                             />}
                     >
                         {t("expenses.DateAndTime")}
