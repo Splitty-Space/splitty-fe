267d2cc187256ac47347b2a736cc3dec28f8bb76
Mikhail Radziukevich <m.radziukevich@dealcloud.com>
2025-04-13 18:17:41 +0200

    Return blue circle

--- Icons/custom/plusIcon.tsx
+++ Icons/custom/plusIcon.tsx
@@ -1,6 +1,6 @@
 export const PlusIcon = ({stroke, style, ...restProps}: { stroke: string, style: any }) => (
     <svg height="100" width="100" xmlns="http://www.w3.org/2000/svg" style={style} {...restProps}>
-        <circle r="45" cx="50" cy="50" fill="#1a1b1c" stroke={stroke} strokeWidth="4"/>
+        <circle r="45" cx="50" cy="50" fill="#1a1b1c" stroke="var(--tgui--button_color)" strokeWidth="4"/>
         <line x1="30" y1="50" x2="70" y2="50" style={{stroke: stroke, strokeWidth: 4}} strokeLinecap="round"/>
         <line x1="50" y1="30" x2="50" y2="70" style={{stroke: stroke, strokeWidth: 4}} strokeLinecap="round"/>
     </svg>
