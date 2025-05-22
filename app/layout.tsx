b299e20a8349ea5f6bebbd6f351e6883bc8062d9
Mikhail Radziukevich <m.radziukevich@dealcloud.com>
2025-05-22 14:24:21 +0200

    Fix meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -276,10 +276,10 @@ export default function RootLayout({children}: Readonly<{
         <Head>
             <title>Splitty</title>
 
-            <meta name="viewport"
-                  content="width=device-width, initial-scale=1, minimum-scale=1, user-scalable=no, shrink-to-fit=no"/>
-
-            {/*<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>*/}
+            <meta
+                name="viewport"
+                content="width=device-width,height=device-height,initial-scale=1,maximum-scale=1,user-scalable=no,shrink-to-fit=no,viewport-fit=cover"
+            />
         </Head>
 
         <body className={classNames("overflow-hidden h-screen", {
