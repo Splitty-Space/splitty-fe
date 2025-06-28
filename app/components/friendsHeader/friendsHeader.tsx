52e401a3fa0ce7bcd9e4b8408844e5a6e2c04413
Nazar Komar <nazarkomar13@gmail.com>
2025-06-28 21:03:06 +0300

    Revert "Update footer"
    This reverts commit 9f9ca9e552ab942097120b1b02e7242a10fa0581.

--- app/components/friendsHeader/friendsHeader.tsx
+++ app/components/friendsHeader/friendsHeader.tsx
@@ -33,6 +33,16 @@ export default function FriendsHeader({searchValue, setSearchValue, onSearchChan
         }
     }, [me, setIsFriendRequestSentSnackbarShown, t]);
 
+    global.window && window.addEventListener("message", (...data) => {
+        console.log("message");
+        console.log(data);
+    });
+
+    global.window && window.addEventListener("shareMessageSent", (...data) => {
+        console.log("shareMessageSent");
+        console.log(data);
+    });
+
     const RightComponent = useCallback(() => (
         <IconButton
             size="l"
