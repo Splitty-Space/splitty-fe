7d7589d0f35b0a3d911c7545819ce9016eb40f43
Nazar Komar <nazarkomar13@gmail.com>
2025-06-28 19:25:03 +0300

    Fix zoom

--- app/layout.tsx
+++ app/layout.tsx
@@ -286,10 +286,7 @@ export default function RootLayout({children}: Readonly<{
         <Head>
             <title>Splitty</title>
 
-            <meta
-                name="viewport"
-                content="width=device-width,height=device-height,initial-scale=1,maximum-scale=1,user-scalable=no,shrink-to-fit=no,viewport-fit=cover"
-            />
+            <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
         </Head>
 
         <body className={classNames("overscroll-none overflow-hidden h-screen", {
