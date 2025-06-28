05e1c73c68219e14e20989912eee2f07036e7fd3
Nazar Komar <nazarkomar13@gmail.com>
2025-06-28 22:59:51 +0300

    Update footer

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -130,7 +130,7 @@ export default function Footer({
         }
     }, [pathname, setSearchValue, setSelectedExpense, friends, setSelectedFriends, selectedUserId, setSelectedUserId, router]);
 
-    const isKeyboardOpen = useDetectKeyboardOpen(100);
+    const isKeyboardOpen = useDetectKeyboardOpen(400);
 
     return (
         <footer className={classNames("footer absolute h-20 w-full", { "hidden": isKeyboardOpen })}>
