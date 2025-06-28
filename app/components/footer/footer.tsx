a8b7947af313d35555d93b2d21b99e0721197df0
Nazar Komar <nazarkomar13@gmail.com>
2025-06-28 23:24:15 +0300

    Update footer

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -13,6 +13,7 @@ import Avatar from "@/app/components/avatar/Avatar";
 import {useStore} from "@/app/store";
 import "./footer.css";
 import classNames from "classnames";
+import {global as globalStyled} from "styled-jsx/css";
 
 interface Tab {
     id: number;
@@ -129,13 +130,13 @@ export default function Footer({
         }
     }, [pathname, setSearchValue, setSelectedExpense, friends, setSelectedFriends, selectedUserId, setSelectedUserId, router]);
 
-    const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+    const [windowHeight, setWindowHeight] = useState(global.window && window.innerHeight);
     const [isKeyboardOpen, setIsKeyboardOpen] = useState(false);
 
     useEffect(() => {
         const handleFocus = () => {
             setTimeout(() => {
-                const currentHeight = window.innerHeight;
+                const currentHeight = global.window && window.innerHeight;
                 const heightDifference = windowHeight - currentHeight;
                 setIsKeyboardOpen(heightDifference > 150);
             }, 300); // Small delay to let keyboard animation complete
@@ -162,7 +163,7 @@ export default function Footer({
     }, [windowHeight]);
 
     useEffect(() => {
-        setWindowHeight(window.innerHeight);
+        setWindowHeight(global.window && window.innerHeight);
     }, []);
 
     return (
