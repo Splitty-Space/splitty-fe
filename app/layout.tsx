df80969ceda2cb267f2b30553cb945a43683c948
Nazar Komar <nazarkomar13@gmail.com>
2025-06-28 15:09:55 +0300

    Fix zoom

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ import useMe from "@/services/useMe";
 import {DARK, DEFAULT_THEME, THEME_TYPE} from "@/const/theme";
 import {ANDROID, DEFAULT_PLATFORM, IOS, PLATFORM_TYPE} from "@/const/platform";
 import {AppRootContext} from "./AppRootContext";
-import {backButton, closingBehavior, miniApp, swipeBehavior, postEvent} from "@telegram-apps/sdk";
+import {backButton, closingBehavior, miniApp, swipeBehavior, viewport, postEvent} from "@telegram-apps/sdk";
 import {retrieveLaunchParams} from "@telegram-apps/bridge";
 import {LocalizationProvider} from "@mui/x-date-pickers";
 import {createTheme, ThemeProvider} from "@mui/material/styles";
